3fd070af7eea782b9f6ef21939f56b9a647ebbec
Seongsu Keum <kss6609@naver.com>
2024-10-30 20:18:36 +0900

    good luck

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import random
 
+st.header("Good Luck!")
+
 for col, number in zip(st.columns(6), sorted(random.sample(range(1, 46), k=6))):
     with col:
         st.header(f"{number}")
