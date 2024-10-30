761ba8efc1f43c488f66ddc30b8aad5cc150cfef
Seongsu Keum <kss6609@naver.com>
2024-10-30 20:45:13 +0900

    h2 tag

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.header("Good Luck!")
 
 for col, number in zip(st.columns(6), sorted(random.sample(range(1, 46), k=6))):
     with col:
-        st.html(f"<h1><center>{number}</center></h1>")
+        st.html(f"<h2><center>{number}</center></h2>")
 
 st.button("Roll", type="primary", use_container_width=True)
