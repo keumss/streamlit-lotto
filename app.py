a7b0efc9a496b03d713fce1ac1384b41c8845852
Seongsu Keum <kss6609@naver.com>
2024-10-30 20:35:52 +0900

    centered numbers

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.header("Good Luck!")
 
 for col, number in zip(st.columns(6), sorted(random.sample(range(1, 46), k=6))):
     with col:
-        st.header(f"{number}")
+        st.html(f"<h1><center>{number}</center></h1>")
 
 st.button("Roll", type="primary", use_container_width=True)
